6db49cbf78890a31ec5b6024c5042825ca9c3229
Manhtroll213 <130965194+Manhtroll213@users.noreply.github.com>
2023-11-06 16:38:17 +0700

    Update Implement methods add, size in DLinkedList.cpp

--- Implement methods add, size in DLinkedList.cpp
+++ Implement methods add, size in DLinkedList.cpp
@@ -1,4 +1,44 @@
 template <class T>
+class DLinkedList {
+public:
+    class Node; // Forward declaration
+protected:
+    Node* head;
+    Node* tail;
+    int count;
+public:
+    DLinkedList();
+    ~DLinkedList();
+    void    add(const T &e);
+    void    add(int index, const T &e);
+    int     size();
+public:
+    class Node
+    {
+    private:
+        T data;
+        Node *next;
+        Node *previous;
+        friend class DLinkedList<T>;
+
+    public:
+        Node()
+        {
+            this->previous = NULL;
+            this->next = NULL;
+        }
+
+        Node(const T &data)
+        {
+            this->data = data;
+            this->previous = NULL;
+            this->next = NULL;
+        }
+    };
+
+};
+
+template <class T>
 void DLinkedList<T>::add(const T& e) {
     /* Insert an element into the end of the list. */
      if(count == 0){
